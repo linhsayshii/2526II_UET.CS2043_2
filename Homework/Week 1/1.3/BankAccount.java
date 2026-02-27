8c8f0f32c3466d001d77f7f9c2d2ad5af213da09
hoangvietlinh2007 <hoangvietlinh2007@outlook.com>
2026-02-27 22:50:08 +0700

    week1 homework edited

--- Homework/Week 1/1.3/BankAccount.java
+++ Homework/Week 1/1.3/BankAccount.java
@@ -2,17 +2,18 @@ public class BankAccount {
     private String accountNumber;
     private String ownerName;
     private double balance = 0.0;
+    //Trạng thái của tài khoản (accountNumber, ownerName, balance)
 
-    public void openAccount(String accNum, String owner) {
+    public void openAccount(String accNum, String owner) {  //Hành vi mở tài khoản
         this.accountNumber = accNum;
         this.ownerName = owner;
     }
 
-    public void deposit(double amount) {
+    public void deposit(double amount) {    //Hành vi nạp tiền
         this.balance += amount;
     }
 
-    public boolean withdraw(double amount) {
+    public boolean withdraw(double amount) {    //Hành vi rút tiền
         if (this.balance >= amount) {
             this.balance -= amount;
             return true;
@@ -21,7 +22,7 @@ public class BankAccount {
     }
 
     public static void main(String[] args) {
-        BankAccount myAccount = new BankAccount();
+        BankAccount myAccount = new BankAccount();  //Định danh đối tượng myAccount
         myAccount.openAccount("101202303", "Nguyen Van A");
         myAccount.deposit(500.0);
         myAccount.withdraw(150.0);
--- Homework/Week 1/1.3/SmartFan.java
+++ Homework/Week 1/1.3/SmartFan.java
@@ -2,17 +2,18 @@ public class SmartFan {
     private String brand = "Xiaomi";
     private boolean isPowerOn = false;
     private int speedLevel = 0; // Mức từ 1 đến 3
+    //Trạng thái của quạt (brand, isPowerOn, speedLevel)
 
-    public void turnOn() {
+    public void turnOn() {  //Hành vi bật quạt
         this.isPowerOn = true;
     }
 
-    public void turnOff() {
+    public void turnOff() { //Hành vi tắt quạt
         this.isPowerOn = false;
         this.speedLevel = 0;
     }
 
-    public void setSpeed(int newSpeed) {
+    public void setSpeed(int newSpeed) {    //Hành vi thay đổi số của quạt
         // Chỉ đổi tốc độ nếu quạt đang bật
         if (this.isPowerOn == true) { 
             this.speedLevel = newSpeed;
@@ -20,11 +21,11 @@ public class SmartFan {
     }
 
     public static void main(String[] args) {
-        SmartFan livingRoomFan = new SmartFan();
+        SmartFan livingRoomFan = new SmartFan();    //Định danh quạt phòng khách
         livingRoomFan.turnOn();
         livingRoomFan.setSpeed(2);
 
-        SmartFan bedroomFan = new SmartFan();
+        SmartFan bedroomFan = new SmartFan();   //Định danh quạt phòng ngủ
         bedroomFan.setSpeed(3); 
         bedroomFan.turnOn();
     }
